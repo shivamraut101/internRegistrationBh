000db9ee056f851171db4a2c6ba4b037838c24a2
shivamraut101 <shivamkumarraut12@gmail.com>
2024-03-14 23:46:51 +0530

    router and auth0 added

--- intern-registration/src/main.jsx
+++ intern-registration/src/main.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// Import Bootstrap styles
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Import Bootstrap JavaScript and Popper.js
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import { GoogleAuth } from './GoogleAuth.jsx';
+import { Auth0Provider, useAuth0  } from '@auth0/auth0-react';
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<GoogleAuth/>
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+<Auth0Provider
+    domain="dev-x04tf7uf3dce71s4.us.auth0.com"
+    clientId="NulSlhId7FbB7Ldc6b8XktfLo3Ts2DHu"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <RouterProvider router={router} />
+    
+  </Auth0Provider>,
+    
   </React.StrictMode>,
 )
